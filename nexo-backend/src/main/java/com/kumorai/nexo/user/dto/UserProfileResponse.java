a234cd486e9bdcf03228b137e2ddf6416a5454ef
DrkGodEater9 <alexander.moralesujueta@gmail.com>
2026-04-27 10:45:45 -0500

    commit de sonarqube

--- nexo-backend/src/main/java/com/kumorai/nexo/user/dto/UserProfileResponse.java
+++ nexo-backend/src/main/java/com/kumorai/nexo/user/dto/UserProfileResponse.java
@@ -4,15 +4,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public record UserProfileResponse(
-        Long id,
-        String email,
-        String nickname,
-        boolean active,
-        LocalDateTime createdAt,
-        List<String> roles,
-        // Semantic student Info added below
-        String studentCode,
-        String entrySemester,
-        String faculty,
-        String career
-) {}
+                Long id,
+                String email,
+                String nickname,
+                boolean active,
+                LocalDateTime createdAt,
+                List<String> roles,
+                // Semantic student Info added below a
+                String studentCode,
+                String entrySemester,
+                String faculty,
+                String career) {
+}
